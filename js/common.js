a6fa6febe29c9309c1a384ea087952adf6ceb584
songyouyoung <dbdud629@naver.com>
2023-09-26 13:41:16 +0900

    [Feat] 첫 커밋
    20230926

---
+++ js/common.js
@@ -0,0 +1,43 @@
+///////////////////////////////////////////////
+/////////////////swiper////////////////////////
+///////////////////////////////////////////////
+setTimeout(function(){
+    $('.item_desc_none').eq(1).css({display: 'none'});
+    $('.m_b_item_desc').eq(1).slideDown(500);
+    var swiper_main_banner = new Swiper(".main_banner", {
+        observer: true,
+        observeParents: true,
+        slidesPerView: 3,
+        slidesPerGroup: 1,
+        spaceBetween: 10,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        keyboard: true,
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false
+        },
+        speed: 500,
+        on: {
+            slideNextTransitionEnd: function () {
+                for(let i=0; i<$('.main_banner .swiper-slide').length; i++){
+                    $('.main_banner .item_desc_none').eq(i).css({display: 'block'});
+                    $('.m_b_item_desc').eq(i).slideUp(500);
+                }
+                $('.item_desc_none').eq(this.activeIndex+1).css({display: 'none'});
+                $('.m_b_item_desc').eq(this.activeIndex+1).slideDown(300);
+            },
+            slidePrevTransitionEnd: function () {
+                for(let i=0; i<$('.main_banner .swiper-slide').length; i++){
+                    $('.main_banner .item_desc_none').eq(i).css({display: 'block'});
+                    $('.m_b_item_desc').eq(i).slideUp(500);
+                }
+                $('.main_banner .item_desc_none').eq(this.activeIndex+1).css({display: 'none'});
+                $('.m_b_item_desc').eq(this.activeIndex+1).slideDown(300);
+            }
+        }
+    });
+}, 10);
